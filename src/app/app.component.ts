f4c3dcf96721c4bb0774fa71d9de8e819fc02bca
Ufuoma <ufuomaovo@gmail.com>
2025-08-12 15:54:51 -0600

    Create Angular Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './componenents/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+    <app-header />
+    <main>
+      <app-home />
+    </main>
+    `
+    ,
+  styles: [`
+    main {
+      padding: 16px;
+    }
+    `],
 })
 export class AppComponent {
   title = 'first-ng-app';
