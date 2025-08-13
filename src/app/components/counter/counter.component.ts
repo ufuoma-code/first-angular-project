e991f620f2b3d4ce39fc9f5fec224ef647240176
Ufuoma <ufuomaovo@gmail.com>
2025-08-13 15:26:23 -0600

    Create Counter Components in Angular

---
+++ src/app/components/counter/counter.component.ts
@@ -0,0 +1,22 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss'
+})
+export class CounterComponent {
+  counterValue = signal(0);
+  increment() {
+    this.counterValue.update((Val) => Val + 1);
+  }
+
+  decrement() {
+    this.counterValue.update((Val) => Val - 1);
+  }
+
+  reset() {
+    this.counterValue.set(0);
+  }
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { GreetingComponent } from '../componenets/greeting/greeting.component';
+import { CounterComponent } from '../components/counter/counter.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [GreetingComponent],
+  imports: [GreetingComponent,CounterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
