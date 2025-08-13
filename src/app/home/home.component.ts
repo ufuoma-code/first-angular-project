f2ed4ce110bd63e3c96cdfd66669196e6c449c5a
Ufuoma <ufuomaovo@gmail.com>
2025-08-13 14:21:22 -0600

    Implement Event Listeners in Angular Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,10 +4,16 @@ import { GreetingComponent } from '../componenets/greeting/greeting.component';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [GreetingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   homeMessage = signal('hello, World!');
-}
+
+ keyUpHandler(event: KeyboardEvent) {
+    console.log(`user pressed the ${event.key} key`);
+  }
+ }
+
