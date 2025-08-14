44692102f82d720f6b0765f882bf54af9f5414f3
Ufuoma <ufuomaovo@gmail.com>
2025-08-13 18:05:10 -0600

    Create Route in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './componenents/header/header.component';
+import { HeaderComponent } from './componenents/header/header.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
     <app-header />
     <main>
-      <app-home />
+     <router-outlet />
     </main>
-    `
-    ,
-  styles: [`
-    main {
-      padding: 16px;
-    }
-    `],
+    `,
+  styles: [
+   `
+   main {
+     padding: 16px;
+   }
+  `,
+  ]
+    
 })
 export class AppComponent {
   title = 'first-ng-app';
--- src/app/componenents/header/header.component.ts
+++ src/app/componenents/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
 title = 'My first Angular app';
