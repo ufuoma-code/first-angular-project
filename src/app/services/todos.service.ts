7cefdebe1918e3e65ca3c7f89a48a54aad2633ff
Ufuoma <ufuomaovo@gmail.com>
2025-08-13 22:08:15 -0600

    Make HTTPS Calls in Angular Services

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,24 +1,14 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Todo } from '../model/todo.type';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodosService {
-todoItems: Array<Todo> = [
-  {
-  title: 'groceries', 
-  id: 0,
-  userId: 1,
-  completed: false,
-
-}, 
-{
-  title: 'car wash',
-  id: 1,
-  userId: 1,
-  completed: false,
-},
-];
-  constructor() { }
+  http = inject(HttpClient);
+getTodosFromApi() {
+    const url = `https://jsonplaceholder.typicode.com/todos`
+    return this.http.get<Array<Todo>>(url);
+  }
 }
